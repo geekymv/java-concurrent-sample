aee3ff6a092855ee4bc2eebf35429d4ee42fc5f2
geekymv <ym2011678@foxmail.com>
2021-04-09 16:03:14 +0800

    两个线程交替输出

---
+++ src/main/java/com/geekymv/concurrent/chapter06/AlternateOutputDemo.java
@@ -0,0 +1,56 @@
+package com.geekymv.concurrent.chapter06;
+
+/**
+ * 两个线程交替输出
+ * A1B2C3D4E5F6G7
+ */
+public class AlternateOutputDemo {
+
+    private static Object lock = new Object();
+
+    public static void main(String[] args) {
+
+        char[] aArr = "ABCDEFG".toCharArray();
+        char[] cArr = "1234567".toCharArray();
+
+
+        Thread t1 = new Thread(()-> {
+            print(aArr);
+        }, "t1");
+
+        Thread t2 = new Thread(()-> {
+            print(cArr);
+        }, "t2");
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void print(char[] c) {
+        synchronized (lock) {
+            for(int i = 0, len = c.length; i < len; i++ ) {
+                System.out.print(c[i]);
+
+                lock.notify();
+
+                if(i != len-1) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+
+}
